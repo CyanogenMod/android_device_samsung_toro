7844661c24a9baa1226c88844417d80d4ae1a3ba
Brint E. Kriebel <github@bekit.net>
2011-12-16 18:59:55 -0700

    toro: Add cm.mk
    Change-Id: I567bf64f8db7e87d2924f6152b4175d741bd0b73

---
+++ cm.mk
@@ -0,0 +1,17 @@
+# Release name
+PRODUCT_RELEASE_NAME := GN
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/toro/full_toro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := toro
+PRODUCT_NAME := cm_toro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_ID=ICL53F BUILD_DISPLAY_ID=ICL53F BUILD_FINGERPRINT="google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys"
