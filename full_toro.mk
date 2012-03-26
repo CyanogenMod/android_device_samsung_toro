d7e6f6415bc15792f8fdda0df692130fc6ee75c0
Colin Cross <ccross@android.com>
2012-03-26 16:25:50 -0700

    move hardware feature definitions
    Move the hardware feature xml files from frameworks/base/data/etc
    to frameworks/native/data/etc.
    
    Change-Id: I8511edc3193b8e5649341048506f2c5eda7964e7

--- full_toro.mk
+++ full_toro.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES := \
 PRODUCT_COPY_FILES += \
     device/samsung/toro/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
