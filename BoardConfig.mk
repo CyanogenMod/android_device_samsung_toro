670d8280ad0996f083add9dcdbcf7c9d22fe5158
Wink Saville <wink@google.com>
2012-08-20 16:27:45 -0700

    Add support for per device bluetooth configuration.
    Bug: 6837125
    Change-Id: I6f159cdd998e27b9d624ffda9c0cb12028d4afe6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,12 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Default values, possibly overridden by BoardConfigVendor.mk
+TARGET_BOARD_INFO_FILE := device/samsung/toro/board-info.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/toro/bluetooth
+
 # Use the non-open-source part, if present
 -include vendor/samsung/toro/BoardConfigVendor.mk
 
 # Use the open-source board-info file if none was set by the vendor
 # config.
-TARGET_BOARD_INFO_FILE ?= device/samsung/toro/board-info.txt
 
 # Use the part that is common between all tunas
 include device/samsung/tuna/BoardConfig.mk
