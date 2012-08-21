ee1862c2c6e3f93548a20542dd798b6f7596cc1c
KalimochoAz <calimochoazucarado@gmail.com>
2012-08-21 14:03:17 +0200

    Update Toro bootanimation.
    Merge after new bootanimation for cm10 is included
    
    Change-Id: I1de26a2995fed07ce0b8a57e1fc93a3b39aaa721

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := GN-CDMA
 
 # Boot animation
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+TARGET_BOOTANIMATION_NAME := vertical-720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
