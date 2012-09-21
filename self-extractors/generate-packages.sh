119642ac78eb647510f030f4ca1a97d8c0cfa47b
Jean-Baptiste Queru <jbq@google.com>
2012-09-21 11:05:26 -0700

    Self-extractors for JRO03R binaries for toro
    Change-Id: I42631ef52a905c2fae8f115b218ef6bf07e0a23b

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -32,6 +32,7 @@
 # 397816 = JRO03B
 # 398337 = JRO03C
 # 405518 = JRO03H
+# 438695 = JRO03R
 # end jb-dev
 BRANCH=jb-dev
 if test $BRANCH=ics-mr1
@@ -41,8 +42,8 @@ then
 fi #ics-mr1
 if test $BRANCH=jb-dev
 then
-  ZIP=mysid-ota-405518.zip
-  BUILD=jro03h
+  ZIP=mysid-ota-438695.zip
+  BUILD=jro03r
 fi # jb-dev
 ROOTDEVICE=toro
 DEVICE=toro
