1f28d6174e4b802dec5a774e2c922769bd7e511d
Rick C <rickfitsnugly@gmail.com>
2012-09-21 11:38:59 -0400

    toro: Update build fingerprint to latest OTA (JRO03O)
    Change-Id: Id8b8a615e1ed3ea91daf415c8e8ec5477cfbe112

--- cm.mk
+++ cm.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys"
