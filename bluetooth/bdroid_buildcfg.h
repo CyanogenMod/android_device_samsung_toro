7306a5b4d84b5ad41197571ce28d339f7d2b1a51
something15525 <something15525@gmail.com>
2013-07-28 17:04:41 -0700

    toro: Fix bluetooth name (again)
    Change-Id: I9deb202a1730306413d09b2a7cae6003460cde2e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy NexusCDMA"
+#define BTM_DEF_LOCAL_NAME   "Galaxy Nexus"
 
 #define BLE_INCLUDED FALSE
 #define BTA_GATT_INCLUDED FALSE
